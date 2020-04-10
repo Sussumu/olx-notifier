3ae24b47ff31f8d4498d7979e008d4253c23a8b5
Gabriel Sussumu Kato <gabrielsussumu@outlook.com>
2020-04-10 13:14:46 -0300

    Adds a Telegram Bot Api which will be used to notififcations. Starts a swagger url.

---
+++ OlxNotifier.TelegramBot/Controllers/WebhookController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+
+namespace OlxNotifier.TelegramBot.Controllers
+{
+    [ApiController]
+    [Route("webhook")]
+    public class WebhookController : ControllerBase
+    {
+        public HttpClient Client { get; }
+
+        public WebhookController(HttpClient client)
+        {
+            Client = client ?? throw new System.ArgumentNullException(nameof(client));
+        }
+
+        [HttpPut]
+        [Route("")]
+        public IActionResult Set([FromBody]string url)
+        {
+
+
+            return Ok();
+        }
+    }
+}
---
+++ OlxNotifier.TelegramBot/Startup.cs
@@ -0,0 +1,54 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+namespace OlxNotifier.TelegramBot
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Telegram Bot API", Version = "v1" });
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Telegram Bot API");
+            });
+        }
+    }
+}
